c2bbcaa2a70d2d620eb8abf1009952afd1493e8a
Mo7ammedd <medo.mostafa22255@gmail.com>
2024-11-21 11:38:42 +0200

    Add ApplicationServicesExtensionsApplicationServicesExtensions

---
+++ HealthTracker.APIs/Extensions/ApplicationServicesExtensionsApplicationServicesExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Asp.Versioning;
+using HealthTracker.Core;
+using HealthTracker.Core.IRepositories;
+using HealthTracker.Repository;
+using HealthTracker.Repository.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthTracker.APIs.Extentions;
+
+public static class ApplicationServicesExtensionsApplicationServicesExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddControllers();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddDbContext<HealthTrackerDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        });
+        services.AddApiVersioning(options =>
+        {
+            options.ReportApiVersions = true;
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.DefaultApiVersion = new ApiVersion(1, 0);
+            options.ReportApiVersions = true;
+        });
+        return services;
+    }
+}
